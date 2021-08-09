e52e4f63291a93565e6a2c6dcefb5ae36e4e0227
Givantha C <givantha.chamara@hitachi-dps.com>
2021-08-09 23:40:14 +0530

    initial project structure

---
+++ src/main/java/com/putchar/jwtgenvalidate/JwtgenvalidateApplication.java
@@ -0,0 +1,13 @@
+package com.putchar.jwtgenvalidate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtgenvalidateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtgenvalidateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/putchar/jwtgenvalidate/JwtgenvalidateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.putchar.jwtgenvalidate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtgenvalidateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
