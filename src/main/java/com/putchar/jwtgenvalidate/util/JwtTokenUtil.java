07905d91238d0868faa94250b98d5ff1082ae2c3
Givantha C <givantha.chamara@hitachi-dps.com>
2021-08-12 05:54:03 +0530

    adding create and validate token methods

--- src/main/java/com/putchar/jwtgenvalidate/util/JwtTokenUtil.java
+++ src/main/java/com/putchar/jwtgenvalidate/util/JwtTokenUtil.java
@@ -2,13 +2,17 @@ package com.putchar.jwtgenvalidate.util;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 @Component
 public class JwtTokenUtil implements Serializable
@@ -46,6 +50,29 @@ public class JwtTokenUtil implements Serializable
 	private Claims getAllClaimsFromToken(String token)
 	{
 		return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
+	}
+
+	public String generateToken(UserDetails userdetails) 
+	{
+		Map<String, Object> claims = new HashMap<String, Object>();
+		return doGenerateToken(claims, userdetails.getUsername());
+	}
+	
+	private String doGenerateToken(Map<String, Object> claims, String subject) 
+	{
+		return Jwts.builder()
+				.setClaims(claims)
+				.setSubject(subject)
+				.setIssuedAt(new Date(System.currentTimeMillis()))
+				.setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY*1000))
+				.signWith(SignatureAlgorithm.HS512, secretKey)
+				.compact();
+	}
+	
+	public boolean validateToken(String token, UserDetails userDetails) 
+	{
+		final String  username = getusernameFromToken(token);
+		return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
 	}
 
 }
